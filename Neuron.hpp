aea2d02b5767acf74af845a3227e2a0432e8b336
metageek <francis@thibault.org>
2020-01-19 17:10:28 -0500

    Moving the inline functions into the class def

--- Neuron.hpp
+++ Neuron.hpp
@@ -13,48 +13,26 @@ private:
 	const Layer layerType;
 	float value;
 	const bool last;
+
+        float calculate(float weight, float inputValue, bool doSigmoid) {
+                return doSigmoid ?
+                        sigmoid(value + (weight * inputValue)) :
+                        value + (weight * inputValue);
+        }
 public:
-	Neuron(Layer layerType, bool last);
-	void calculateValue(float weight, float inputValue, bool doSigmoid);
-	float sigmoid(float value);
-	void setValue(float value);
-	float getValue();
-	bool isLast();
+	Neuron(Layer _layerType, bool _last)
+          : layerType(_layerType), value(0), last(_last) {
+        }
+        void calculateValue(float weight, float inputValue, bool doSigmoid) {
+          value = calculate(weight, inputValue, doSigmoid);
+        }
+        static float sigmoid(float value) { return 1 / (1 + exp(-value)); }
+        void setValue(float _value) { value = _value; }
+	float getValue() const { return value; }
+        bool isLast() const { return last; }
 };
 
 using Vector = std::vector<Neuron*>;
-
-inline Neuron::Neuron(Layer _layerType, bool _last)
-  : layerType(_layerType), value(0), last(_last) {
-}
-
-inline void Neuron::calculateValue(float weight, float inputValue, bool doSigmoid)
-{
-	if(doSigmoid)
-		this->value = this->sigmoid(this->value + (weight * inputValue));
-	else
-		this->value = this->value + (weight * inputValue);
-}
-
-inline float Neuron::sigmoid(float value)
-{
-	return 1 / (1 + exp(-value));
-}
-
-inline void Neuron::setValue(float value)
-{
-	this->value = value;
-}
-
-inline float Neuron::getValue()
-{
-	return this->value;
-}
-
-inline bool Neuron::isLast()
-{
-	return this->last;
-}
 
 } // end namespace NN
 
