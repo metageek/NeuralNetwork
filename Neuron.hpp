56d732313a7fb309252d947f32e0a096055965a5
metageek <francis@thibault.org>
2020-01-19 17:39:18 -0500

    More reformatting

--- Neuron.hpp
+++ Neuron.hpp
@@ -10,27 +10,27 @@ namespace NN {
 
 class Neuron {
 private:
-	const Layer layerType;
-	float value;
-	const bool last;
+  const Layer layerType;
+  float value;
+  const bool last;
 
-        float calculate(float weight, float inputValue, bool doSigmoid) {
-          return doSigmoid ?
-            sigmoid(value + (weight * inputValue)) :
-            value + (weight * inputValue);
-        }
+  float calculate(float weight, float inputValue, bool doSigmoid) {
+    return doSigmoid ?
+      sigmoid(value + (weight * inputValue)) :
+      value + (weight * inputValue);
+  }
 
 public:
-	Neuron(Layer _layerType, bool _last = false)
-          : layerType(_layerType), value(0), last(_last) {
-        }
-        void calculateValue(float weight, float inputValue, bool doSigmoid) {
-          value = calculate(weight, inputValue, doSigmoid);
-        }
-        static float sigmoid(float value) { return 1 / (1 + exp(-value)); }
-        void setValue(float _value) { value = _value; }
-	float getValue() const { return value; }
-        bool isLast() const { return last; }
+  Neuron(Layer _layerType, bool _last = false)
+    : layerType(_layerType), value(0), last(_last) {
+  }
+  void calculateValue(float weight, float inputValue, bool doSigmoid) {
+    value = calculate(weight, inputValue, doSigmoid);
+  }
+  static float sigmoid(float value) { return 1 / (1 + exp(-value)); }
+  void setValue(float _value) { value = _value; }
+  float getValue() const { return value; }
+  bool isLast() const { return last; }
 };
 
 using Vector = std::vector<Neuron*>;
